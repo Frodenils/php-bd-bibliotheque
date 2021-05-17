e6abff2e911068c4f85dfb48ac387b11b451c700
Frodenils <71440881+Frodenils@users.noreply.github.com>
2021-05-17 16:15:34 +0200

    Delete conf.php

--- conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//Rename this to conf.php
-
-define('DBHOST','mysql-celol-informatique.alwaysdata.net');
-define('DBUSER','230461_cdi');
-define('DBPSWD','Chat-Bite69');
-define('DBNAME','celol-informatique_cdi');
